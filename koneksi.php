2cb3a4789f9ffe29494e4a453199d3fdfcad764f
Vido Rizqy <46133637+vidorizqy47@users.noreply.github.com>
2020-04-05 12:06:54 +0700

    webservice

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id12804385_root');
+ define('PASS','TheLionheart1199#');
+ define('DB','id12804385_tugas');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
